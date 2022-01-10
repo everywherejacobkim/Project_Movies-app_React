654323eb12fbfac507b5251bf305ee964b0bd8c9
Jacob Namhyung Kim <everywherejacobkim@gmail.com>
2022-01-09 19:58:57 -0800

    feat: set voteClass & css styling

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -3,6 +3,16 @@ import "./Movie.css";
 
 const Image_API = "https://image.tmdb.org/t/p/w1280";
 
+const setVoteClass = (vote) => {
+    if(vote >= 8) {
+        return 'green';
+    } else if(vote >= 6) {
+        return 'orange';
+    } else {
+        return 'red';
+    }
+};
+
 const Movie = ({title, poster_path, overview, vote_average}) => (
     <div className="movie">
        
@@ -10,7 +20,8 @@ const Movie = ({title, poster_path, overview, vote_average}) => (
        
        <div className="movie-info">
             <h3>{title}</h3>
-            <div>{vote_average}</div>
+            <div className={`tag ${setVoteClass(vote_average)
+            }`}> {vote_average}</div>
        </div>
 
        <div className="movie-over">
